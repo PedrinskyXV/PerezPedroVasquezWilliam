c8fcd268cdf323d0db12bc1a1975507446715df1
PedrinskyXV <pedro.perez18@itca.edu.sv>
2022-06-11 13:48:20 -0600

    Final AFE

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="vh-100 d-flex justify-content-center align-items-center">                        
+                <div>                    
+                    <img src="{{ asset('img/welcome.svg') }}" alt="BIENVENIDO" class="img-fluid">                    
+                </div>
+                
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PDFController;
+use App\Http\Controllers\BankController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CheckController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\SupplierController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    Route::get('generateSummary/{id}', [PDFController::class, 'generateSummary']);
+
+    Route::resource('api/supplier', SupplierController::class);
+    Route::resource('api/document', DocumentController::class);
+    Route::resource('api/bank', BankController::class);
+
+   
+    Route::get('/suppliers', function () {
+        return view('supplier.index');
+    });
+
+    Route::get('/documents', function () {
+        return view('document.index');
+    });
+
+    Route::get('/banks', function () {
+        return view('bank.index');
+    });
+
+    Route::get('/reports', function () {
+        return view('reports');
+    });
+
+    Route::get('/summary', function () {
+        return view('summary.index');
+    });
+});
+
+
+Auth::routes(['register'=>false, 'reset'=>false]);
