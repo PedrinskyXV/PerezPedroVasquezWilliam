32e9c1137df3324d6a455c6a4ee060f8cb96a794
WilliamVasquez <williamvasquez16@gmail.com>
2022-06-11 15:11:52 -0600

    migrate, seeder un error

--- database/migrations/2022_06_11_142511_create_branches_table.php
+++ database/migrations/2022_06_11_142511_create_branches_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBranchesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('branches', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name',25)->unique();
+            $table->string('address',150);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('branches');
     }
 }
---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Employee;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Employee::insert([
+            [
+                "name" => "PPerez",
+                "age" => 22,
+                "salary_base" => 600,
+                "id_administrador" => 1,
+                "id_branch" => 1,
+                "created_at" => now(),
+            ],
+            [
+                "name" => "William",
+                "age" => 27,
+                "salary_base" => 650,
+                "id_administrador" => 2,
+                "id_branch" => 2,
+                "created_at" => now(),
+            ],
+            [
+                "name" => "Tzec",
+                "age" => 30,
+                "salary_base" => 850,
+                "id_administrador" => 1,
+                "id_branch" => 2,
+                "created_at" => now(),
+            ],
+            [
+                "name" => "Oscar",
+                "age" => 29,
+                "salary_base" => 1200,
+                "id_administrador" => 3,
+                "id_branch" => 3,
+                "created_at" => now(),
+            ],
+        ]);
+    }
+}
